bcb9a7c68d8e321fcafd93a571c9332aac0c46e8
tvaltn <valtniemi@gmail.com>
2024-09-20 09:58:31 +0200

    task done

--- src/advanced/strings-adv.js
+++ src/advanced/strings-adv.js
@@ -3,25 +3,25 @@ const cityStr = 'City'
 const shoppingList = 'apples, bananas, oranges, grapes'
 
 // TODO get the INDEX of the character where 'York' word is found in the city variable
-const cityIndex = 0
+const cityIndex = city.indexOf('York')
 
 // TODO - get the substring "York" from the city variable
-const citySubstring = ''
+const citySubstring = city.substring(cityIndex)
 
 // TODO = replace 'York' with 'Delhi'
-const cityReplaced = ''
+const cityReplaced = city.replace('York', 'Delhi')
 
 // TODO - check if city starts with 'New' and ends with 'York'
-const cityStartsWith = false
-const cityEndsWith = false
+const cityStartsWith = city.startsWith('New')
+const cityEndsWith = city.endsWith('York')
 
 // TODO - split the shopping list into an array of items without spaces
 // hint (you might want to replace first, then split)
-const shoppingListArray = []
+const shoppingListArray = shoppingList.replace(/ /g, '').split(',')
 
 // TODO - concatenate two strings using the `${var}` syntax
 // make cityStrConcat equal to 'New York City'
-const cityStrConcat = ''
+const cityStrConcat = `${city} ${cityStr}`
 
 module.exports = {
   cityIndex,
